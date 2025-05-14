9a1ba29acea848e64bcf008e43e59c7cb9942430
shikdershondhi <ahateshamulislam@gmail.com>
2025-05-14 18:23:15 +0600

    refactor: update BudgetPage constructor and streamline navigation in budgetFlow tests

--- pages/budgetPage.ts
+++ pages/budgetPage.ts
@@ -5,16 +5,14 @@ import { BasePage } from './basePage';
 export class BudgetPage extends BasePage {
   readonly budgetMenuItem: Locator;
   readonly dashboardLink: Locator;
-
   constructor(page: Page) {
     super(page);
-    // this.dashboardLink = page.locator("body > div.b-content > nav > ul:nth-child(1) > li:nth-child(3) > ul > li:nth-child(2)");
-    this.dashboardLink = page.getByRole('listitem').filter({ hasText: 'Budget Dashboard Repository' }).getByRole('link').first();
+   
+    // If you want to navigate, call page.goto() in a method, not in the constructor.
   }
 
   async navigateToBudgetDashboard() {
-    await this.dashboardLink.click({ force: true });
-    // await this.budgetMenuItem.click({ force: true });
+    await this.page.goto('https://nsdev24.neustring.com/budget/dashboard/');
     await this.waitForLoadState();
   }
 }
--- tests/e2e/budgetFlow.spec.ts
+++ tests/e2e/budgetFlow.spec.ts
@@ -1,9 +1,13 @@
 import { test } from '@playwright/test';
 import { BudgetPage } from '../../pages/budgetPage';
-
+import * as testData from '../../fixtures/testdata.json';
 // Use test.describe to group related tests
 test.describe('Budget Flow', () => {
-  
+  test.beforeEach(async ({ page }) => {
+    // Navigate to the home page before each test
+    await page.goto(testData.login.Homepage);
+  });
+
   test('Navigate from Budget dashboard', async ({ page }) => {
     const budgetPage = new BudgetPage(page);
     await budgetPage.navigateToBudgetDashboard();
