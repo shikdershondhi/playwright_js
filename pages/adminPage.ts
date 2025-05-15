d290617f0e967e6a814384d56c994e74232b07f3
shikdershondhi <ahateshamulislam@gmail.com>
2025-05-15 15:12:25 +0600

    feat: enhance AdminPage class with user flow methods and update tests for improved navigation

--- pages/adminPage.ts
+++ pages/adminPage.ts
@@ -3,16 +3,47 @@ import { BasePage } from './basePage';
 
 
 export class AdminPage extends BasePage {
-//   readonly budgetMenuItem: Locator;
+   readonly adminButton: Locator;
+   readonly userLink: Locator;
+   readonly searchBoxselect: Locator;
+    readonly userNameFill: Locator;
+    readonly clickSearchButton: Locator;
+    readonly clickTheUserName: Locator;
+    readonly scrolltoview: Locator;
+    readonly selectTextfromList: Locator;
+    readonly Arrowbutton: Locator;
 
 
   constructor(page: Page) {
     super(page);
-   
+    this.adminButton = page.getByRole('link', { name: 'Admin' });
+    //this.userLink = page.getByRole('rowheader', { name: 'Users', exact: true });
+    this.userLink = page.getByRole('link', { name: 'Users', exact: true });
+    this.searchBoxselect = page.getByRole('textbox', { name: 'Search' });
+    this.userNameFill = page.getByRole('textbox', { name: 'Search' });
+    this.clickSearchButton = page.getByRole('button', { name: 'Search' });
+    this.clickTheUserName = page.getByRole('link', { name: 'shikder' });
+    this.scrolltoview = page.locator('#id_groups_from');
+    this.selectTextfromList = page.locator('#id_groups_from',{hasText:'AREX1'});
+    this.Arrowbutton = page.locator('#id_groups_add_link');
+    // await page.pause() // Wait for 5 seconds to ensure the page is loaded
   }
 
-  async navigateToBudgetDashboard() {
-    
-    
+  async navigateToAdminDashboard() {
+    await this.adminButton.click();
+    await this.userLink.click();
+  }
+  async searchAndSelectUser() {
+    await this.searchBoxselect.click();
+    await this.userNameFill.fill('shikder');
+    await this.clickSearchButton.click();
+    await this.clickTheUserName.click();
+  }
+  async selectUserPermission() {
+    await this.scrolltoview.scrollIntoViewIfNeeded();
+    await this.selectTextfromList.selectOption('41');
+    await this.Arrowbutton.click();
+    await this.page.waitForTimeout(5000);
+    // await this.page.pause() // Wait for 5 seconds to ensure the page is loaded
   }
 }
--- tests/e2e/adminUserFlow.spec.ts
+++ tests/e2e/adminUserFlow.spec.ts
@@ -10,18 +10,10 @@ test.describe('Admin Flow', () => {
 
   test('Navigate from admin dashboard', async ({ page }) => {
     const adminPage = new AdminPage(page);
-    await page.getByRole('link', { name: 'Admin' }).click();
-    await page.getByRole('rowheader', { name: 'Users', exact: true }).click();
-    await page.getByRole('link', { name: 'Users', exact: true }).click();
-    await page.getByRole('textbox', { name: 'Search' }).click();
-    await page.getByRole('textbox', { name: 'Search' }).fill('shikder');
-    await page.getByRole('button', { name: 'Search' }).click();
-    await page.getByRole('link', { name: 'shikder' }).click();
-    // await page.locator('#id_groups_from', { hasText: 'AREX1' }).click();
-    await page.locator('#id_groups_from').scrollIntoViewIfNeeded
-    await page.locator('#id_groups_from',{hasText:'AREX1'}).selectOption('41');
-    await page.locator('#id_groups_add_link').click();
-    await page.waitForTimeout(5000); // Wait for 5 seconds to ensure the page is loaded
-    // await page.pause() // Wait for 5 seconds to ensure the page is loaded
+    await adminPage.navigateToAdminDashboard();
+    await adminPage.searchAndSelectUser();
+    await adminPage.selectUserPermission();
+    await page.pause(); // Wait for 5 seconds to ensure the page is loaded
+    await page.screenshot({ path: 'downloads/screenshot/screenshot.png' }); // Take a screenshot
   });
 });
