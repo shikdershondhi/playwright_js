9b8fac4229aff5e7bba65b0cca69bd8b859229e1
shikdershondhi <ahateshamulislam@gmail.com>
2025-05-26 15:30:10 +0600

    refactor: restructure testdata.json and update LoginPage to use new data format

--- tests/setup/auth.setup.ts
+++ tests/setup/auth.setup.ts
@@ -1,15 +1,12 @@
 import { test as setup } from '@playwright/test';
 import { LoginPage } from '../../pages/login.page';
-import * as testData from '../../fixtures/testdata.json';
 
-setup('authenticate', async ({ page }) => {
+setup('authenticate with valid user login with session', async ({ page }) => {
   const loginPage = new LoginPage(page);
-  await loginPage.page.goto(testData.login.url);
-  await loginPage.login(testData.login.username, testData.login.password);
-
-    // Wait for navigation to complete after login
+  await loginPage.navigate();
+  await loginPage.login();
+  // Wait for navigation to complete after login
   await page.waitForURL(/worldmap/);
-  
   // Save authenticated state to a file
   await page.context().storageState({ 
     path: 'fixtures/auth/userAuthState.json' 
