7f422a99fe3d24ecea22175a46d351f1ba4bcd16
shikdershondhi <ahateshamulislam@gmail.com>
2025-05-27 15:00:11 +0600

    fixture env data added

--- utils/env.ts
+++ utils/env.ts
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv';
 import path from 'path';
+import * as fs from "node:fs";
 
 const envFile = `.env.${process.env.NODE_ENV || 'dev'}`;
 dotenv.config({ path: path.resolve(__dirname, '../env', envFile) });
 
 export const BASE_URL = process.env.BASE_URL || 'https://nsdev24.neustring.com/';
+
+const testDataPath = path.resolve(__dirname, `../fixtures/${process.env.NODE_ENV || 'dev'}.json`);
+export const TEST_DATA = JSON.parse(fs.readFileSync(testDataPath, 'utf-8'));
